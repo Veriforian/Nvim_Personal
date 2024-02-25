c6aa6cec3a683eadc5bc9609e22b1a823edaa6c8
Veri <benjones14480@gmail.com>
2024-02-25 12:41:51 -0700

    Adding gitsigns

--- lua/benjones/packer.lua
+++ lua/benjones/packer.lua
@@ -34,7 +34,8 @@ return require('packer').startup(function(use)
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
   use('m4xshen/autoclose.nvim')
-
+  use('lewis6991/gitsigns.nvim')
+  
   use {
 	  "VonHeikemen/lsp-zero.nvim",
 	  branch = "v3.x",
