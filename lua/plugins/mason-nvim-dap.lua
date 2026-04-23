6d2d0c62a45aafc6fb936ac12348cb3904e641fb
Ben Jones <benjones14480@gmail.com>
2026-04-23 05:17:21 -0400

    add go to mason

--- lua/plugins/mason-nvim-dap.lua
+++ lua/plugins/mason-nvim-dap.lua
@@ -17,6 +17,7 @@ return {
       -- Update this to ensure that you have the debuggers for the langs you want
       "js",
       "stylelua",
+      "go",
     },
   },
   -- mason-nvim-dap is loaded when nvim-dap loads
