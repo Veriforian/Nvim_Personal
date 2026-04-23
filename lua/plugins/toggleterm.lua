ffd5a5ac12e069bfb63b9820a13d483948f45dee
Ben Jones <benjones14480@gmail.com>
2026-04-23 05:19:32 -0400

    feat: custom toggleterm setup, <C-/> toggles last open terminal

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,7 +2,56 @@ return {
   "akinsho/toggleterm.nvim",
   lazy = true,
   cmd = { "ToggleTerm" },
+  init = function()
+    -- Most recent terminal tracking
+    vim.api.nvim_create_autocmd({ "BufEnter", "TermEnter" }, {
+      pattern = "term://*",
+      callback = function()
+        if vim.b.toggle_number then
+          vim.g.last_active_term_id = vim.b.toggle_number
+        end
+      end,
+    })
+  end,
   keys = {
+    {
+      "<c-_>",
+      function()
+        if vim.bo.buftype == "terminal" and vim.b.toggle_number then
+          require("toggleterm").toggle(vim.b.toggle_number)
+          return
+        end
+
+        local terms = require("toggleterm.terminal").get_all()
+        if #terms == 0 then
+          -- open new floatterm
+          require("toggleterm").toggle(1, 0, LazyVim.root.get(), "float")
+          return
+        end
+
+        -- Find most recent term to toggle
+        local target_id = vim.g.last_active_term_id
+        if not target_id then
+          terms[1]:toggle()
+          return
+        end
+
+        local found = false
+        for _, t in ipairs(terms) do
+          if t.id == target_id then
+            found = true
+            t:toggle()
+            break
+          end
+        end
+
+        if not found then
+          terms[1]:toggle()
+        end
+      end,
+      mode = { "n", "t", "i" },
+      desc = "Toggle last focused terminal",
+    },
     {
       "<leader>tf",
       function()
@@ -61,7 +110,6 @@ return {
         return vim.o.columns * 0.4
       end
     end,
-    open_mapping = [[<c-_>]],
     hide_numbers = true, -- hide the number column in toggleterm buffers
     shade_filetypes = {},
     shade_terminals = false,
